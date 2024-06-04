76d9694046a28f9cf0eb62a4a964e7635dd61a8b
chanwooLim <hvcloc@gmail.com>
2024-06-04 09:49:13 +0900

    # update interface & update result

--- pages/03_Result.py
+++ pages/03_Result.py
@@ -54,7 +54,28 @@ with col2:
             전기차 보급 확대 정책에 따라 기업들이 사업용 전기차를 늘리고 있는 추세로,
             주간형 지역은 사업용 전기차 비율이 상대적으로 높다는 것을 알 수 있습니다.
             이에 따라, 주간형 지역에 전기차 충전기를 설치하는 것이 타당합니다.
+        """
+        )
 
+st.write("---")
+
+col1, col2 = st.columns([3, 4])
+with col1:
+    with st.container():
+        st.image("image/주거면적_산업면적.png", use_column_width=True)
+        st.image("image/유형별_면적대비_아파트수.png", use_column_width=True)
+        st.image("image/유형별_노후_아파트_비율.png", use_column_width=True)
+        st.image("image/주차장확보율.png", use_column_width=True)
+
+with col2:
+    with st.container():
+        st.write(
+            """
+            ##### 가설 1: 인구 수, 소득, 사업용 전기차 수 등의 요인으로 경제 활동을 중심으로 하는 자치구에 전기차 충전기를 설치하는 것이 더 효율적  
+            
+            전기차 보급 확대 정책에 따라 기업들이 사업용 전기차를 늘리고 있는 추세로,
+            주간형 지역은 사업용 전기차 비율이 상대적으로 높다는 것을 알 수 있습니다.
+            이에 따라, 주간형 지역에 전기차 충전기를 설치하는 것이 타당합니다.
         """
         )
 
